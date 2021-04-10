13803828e177ea36aad0d8bed87f3f8fccc166d1
sProDev <suluh.webdevelopers@hotmail.com>
2021-04-10 13:00:55 +0700

    :yum: added pwa support

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,11 @@
 <script>
     import { Router } from "@roxi/routify";
     import { routes } from "../.routify/routes";
+
+    import Serviceworker from "./Serviceworker.svelte";
 </script>
+
+<Serviceworker />
 
 <Router {routes} />
 
